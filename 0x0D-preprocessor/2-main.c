10ee98b1fb9c7a219254eda90dee1c1dcc701b7d
Shamoche <alakindeakintomiwa@gmail.com>
2023-04-17 09:09:57 -0700

    function

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Include standard input-output library
 
 int main() {
-    printf("%s\n", __FILE__);
-    return 0;
+    printf("%s\n", __FILE__); // Print the name of the file using __FILE__ macro and a newline character
+    return 0; // Return 0 to indicate successful completion of the program
 }
 
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#include <stdio.h> // For standard input-output operations
+#include <stdlib.h> // For the abs() function used in the example
+
+#define ABS(x) ( ( (x) < 0 ) ? -(x) : (x) )
+
