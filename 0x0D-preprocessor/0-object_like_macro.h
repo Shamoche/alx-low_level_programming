090487b12bd75269faa99785d95f30ddfe0daa07
Shamoche <alakindeakintomiwa@gmail.com>
2023-04-17 08:59:30 -0700

    macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,17 +1,7 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int s;
+#define SIZE 1024
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
-}
+#endif /* HEADER_FILE_H */
+
