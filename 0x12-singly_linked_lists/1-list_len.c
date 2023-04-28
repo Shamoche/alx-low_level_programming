9e9790bb2040860286d78aedad46375c66297a9a
Alakinde Samuel <alakindeakintomiwa@gmail.com>
2023-04-27 21:50:12 -0700

    1-list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - Calculate the number of elements.
- * @h: Pointer to a list.
- * Return: Integer.
- **/
+ * list_len - calculte the number of elements in a linked list.
+ * @h: head of the liked list.
+ *
+ * Return: number of elements in the linked list.
+ */
 
 size_t list_len(const list_t *h)
 {
-	const list_t *temp;
-	unsigned int counter = 0;
+	size_t count = 0;
 
-	temp = h;
-	while (temp)
+	while (h != NULL)
 	{
-		counter++;
-		temp = temp->next;
+		count++;
+		h = h->next;
 	}
-	return (counter);
+	return (count);
 }
