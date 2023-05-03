3dfea76455dcca8333f50c69411d6ce0f4006ade
Alakinde Samuel <alakindeakintomiwa@gmail.com>
2023-05-03 06:34:22 -0700

    advanced task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,33 +9,23 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	const listint_t *slow = head, *fast = head;
+	long int diff;
 
-	while (fast && fast->next && fast->next < fast)
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		diff = head - head->next;
 		num++;
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-
-	if (fast && fast->next)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		num++;
-		slow = slow->next;
-		fast = fast->next;
-		printf("-> [%p] %d\n", (void *)fast, fast->n);
-		num++;
-	}
-
-	while (slow)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		num++;
-		slow = slow->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
 
 	return (num);
 }
+
 
