89d7c298b02fa62afe71222f4891caaa6860ecbb
Shamoche <alakindeakintomiwa@gmail.com>
2023-08-25 13:18:06 -0700

    singly linked

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
-
-section .text
-    global main
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-    pop rbp
-
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; status: 0
-    syscall
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
