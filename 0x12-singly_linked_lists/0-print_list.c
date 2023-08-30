0b32efc2c89ef0d8edd3d2722437050a85e9df17
Samuel <alakindeakintomiwa@gmail.com>
2023-08-30 10:26:29 +0100

    SLL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,33 +1,24 @@
-/*
- * File: 0-print_list.c
- * Auth: Gedeon Obae Gekonge
- */
-
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * print_list - prints to standard output
+ * @h: A node in list_t
+ * Return: The number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t num_of_nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		num_of_nodes++;
 	}
-
-	return (nodes);
+	return (num_of_nodes);
 }
